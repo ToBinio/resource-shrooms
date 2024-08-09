90787d696f4af930cde646a9deb7b29ce06ad211
tobinio <Tobias.frischmann1@gmail.com>
2024-08-09 17:12:18 +0200

    add translations

--- src/main/java/tobinio/resourceshrooms/datagen/EnglishTranslationProvider.java
+++ src/main/java/tobinio/resourceshrooms/datagen/EnglishTranslationProvider.java
@@ -4,7 +4,9 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.minecraft.registry.RegistryWrapper;
 import tobinio.resourceshrooms.ResourceShrooms;
+import tobinio.resourceshrooms.blocks.ModBlocks;
 import tobinio.resourceshrooms.items.ModItemGroups;
+import tobinio.resourceshrooms.items.ModItems;
 import tobinio.resourceshrooms.mushrooms.Mushroom;
 import tobinio.resourceshrooms.mushrooms.Mushrooms;
 
@@ -27,9 +29,16 @@ public class EnglishTranslationProvider extends FabricLanguageProvider {
         translationBuilder.add("emi.category.%s.mutations".formatted(ResourceShrooms.MOD_ID), "Mushroom Mutations");
 
         translationBuilder.add("tag.item.%s.mushroom".formatted(ResourceShrooms.MOD_ID), "Resourceshrooms");
-        translationBuilder.add("tag.item.%s.mushroom_grow_block".formatted(ResourceShrooms.MOD_ID), "Mushroom Grow Blocks");
-        translationBuilder.add("tag.item.%s.mushroom_stable_block".formatted(ResourceShrooms.MOD_ID), "Mushroom Stable Grow Blocks");
-        translationBuilder.add("tag.item.%s.mushroom_mutation_block".formatted(ResourceShrooms.MOD_ID), "Mushroom Mutation Grow Blocks");
+        translationBuilder.add("tag.item.%s.mushroom_head".formatted(ResourceShrooms.MOD_ID), "Mushroom Heads");
+        translationBuilder.add("tag.item.%s.mushroom_spore".formatted(ResourceShrooms.MOD_ID), "Mushroom Spores");
+
+        translationBuilder.add(ModBlocks.GROUND_TIER1, "Ground Tier1");
+        translationBuilder.add(ModBlocks.GROUND_TIER2, "Ground Tier2");
+        translationBuilder.add(ModBlocks.GROUND_TIER3, "Ground Tier3");
+        translationBuilder.add(ModBlocks.GROUND_TIER4, "Ground Tier4");
+
+        translationBuilder.add(ModItems.MUTAGEN, "Mutagen");
+        translationBuilder.add(ModItems.STABILIZER, "Stabilizer");
     }
 
     public void translateMushroom(TranslationBuilder translationBuilder, Mushroom mushroom) {
